fc676e32fb6f31b073c09e734cca37840f395571
Adam De Angeli <116006840+Adamadem29@users.noreply.github.com>
2023-06-05 16:09:21 +0200

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.sae201 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.sae201 to javafx.fxml;
+    exports com.example.sae201;
+}
