4a309dbc163971caab7d79cb53d401048085ca16
d22004772 <adam.DEANGELI@etu.univ-amu.fr>
2023-06-05 17:18:47 +0200

    Tableaux créé

---
+++ src/main/java/com/example/sae201/SeismeController.java
@@ -0,0 +1,14 @@
+package com.example.sae201;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class SeismeController {
+
+    @FXML
+    private TableView tableau;
+
+    public void initialize(){
+
+    }
+}
