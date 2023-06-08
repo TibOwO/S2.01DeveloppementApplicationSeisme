b46d4f0de5004d6b8a97aed4fe24a53d6638deab
Sarah Ripoche <sarah.ripoche@etu.univ-amu.fr>
2023-06-08 18:24:47 +0200

    Ajout fonctionnement boutons pour changer l'affichage

--- src/main/java/com/example/sae201/HelloController.java
+++ src/main/java/com/example/sae201/HelloController.java
@@ -6,6 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import static com.example.sae201.OuvertureJava2.lSeismes;
@@ -17,6 +21,8 @@ import static com.example.sae201.OuvertureJava2.lSeismes;
 public class HelloController {
 
     @FXML
+    private BorderPane fenetre;
+    @FXML
     private TableView<Seisme> tableView;
 
     @FXML
@@ -27,6 +33,26 @@ public class HelloController {
     @FXML
     private TextField date;
 
+    @FXML
+    private GridPane graphiques;
+
+    @FXML
+    private HBox carte;
+
+    @FXML
+    protected void handleCarte(){
+        fenetre.setCenter(carte);
+    }
+
+    @FXML
+    protected void handleStats(){
+        fenetre.setCenter(graphiques);
+    }
+
+    @FXML
+    protected void handleTableau(){
+        fenetre.setCenter(tableView);
+    }
     @FXML
     protected void handleRechercher() {
         List<Seisme> tri = filtrerParId(lSeismes, id.getText());
