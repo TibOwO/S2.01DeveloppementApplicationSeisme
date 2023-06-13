ca81968c6cce88bc35b9c472a1725c4bee1184b1
Sarah Ripoche <sarah.ripoche@etu.univ-amu.fr>
2023-06-13 16:44:02 +0200

    Javadoc sur les autres fichiers

--- src/main/java/com/example/sae201/ListSeisme.java
+++ src/main/java/com/example/sae201/ListSeisme.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Sert a eviter d'avoir trop de code dans la classe controlleur. Cela recupere la liste des donnees du CSV
+ * et applique les filtres.
+ */
 public class ListSeisme {
     private List<Seisme> seismeList;
 
@@ -20,6 +24,12 @@ public class ListSeisme {
 
 
     //Les fonctions de recherche
+
+    /**
+     * Sert a rechercher par intensite epicentrale
+     * @param aGarder string de ce que l'on veut chercher
+     * @return liste filtree avec uniquement ce qui nous interesse
+     */
     public ListSeisme filtrerParIntensiteEpicentrale(String aGarder) {
         ListSeisme filteredList = new ListSeisme(this.getSeismeList().stream()
                 .filter(entry -> entry.intensiteEpicentraleProperty().getValue().toString().startsWith(String.valueOf(aGarder)))
@@ -27,6 +37,11 @@ public class ListSeisme {
         return filteredList;
     }
 
+    /**
+     * Sert a rechercher par identifiant
+     * @param aGarder string de ce que l'on veut chercher
+     * @return liste filtree avec uniquement ce qui nous interesse
+     */
     public ListSeisme filtrerParId(int aGarder) {
         ListSeisme filteredList = new ListSeisme(this.getSeismeList().stream()
                 .filter(entry -> entry.idProperty().getValue().toString().startsWith(String.valueOf(aGarder)))
@@ -34,6 +49,11 @@ public class ListSeisme {
         return filteredList;
     }
 
+    /**
+     * Sert a rechercher par date
+     * @param aGarder string de ce que l'on veut chercher
+     * @return liste filtree avec uniquement ce qui nous interesse
+     */
     public ListSeisme filtrerParDate(String aGarder) {
         ListSeisme filteredList = new ListSeisme(this.getSeismeList().stream()
                 .filter(entry -> entry.dateProperty().getValue().toString().startsWith(String.valueOf(aGarder)))
